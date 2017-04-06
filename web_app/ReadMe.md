4173b77c4a77e530dae8171947bad60d331ec154
cheyulin <yche@cse.ust.hk>
2017-04-06 15:56:04 +0800

    ignore node modules add script for add node modules update index.js

---
+++ web_app/ReadMe.md
@@ -0,0 +1,29 @@
+# WebApp Introduction
+## Packages
+
+jquery
+
+```zsh
+npm install jquery
+```
+
+dracula
+
+```zsh
+npm install --save graphdracula raphael
+```
+
+## Tool Usage
+
+add npm module into local `node_modules`
+
+```zsh
+npm install jquery
+```
+
+
+use browserify to resolve dependencies and generate `bundle.js`
+
+```zsh
+browserify index.js -o bundle.js
+```
--- web_app/index.js
+++ web_app/index.js
@@ -2,65 +2,48 @@ var Dracula = require('graphdracula');
 var Graph = Dracula.Graph;
 var Renderer = Dracula.Renderer.Raphael;
 var Layout = Dracula.Layout.Spring;
-var graph = new Graph();
-
 
 /*
-{
-	"0": {
-		"0": ["0,1", "1,2"],
-		"1": ["1,2", "2,3", "3,4"],
-		"2": ["4,6", "4,5"]
-	},
+ {
+ "0": {
+ "0": ["0,1", "1,2"],
+ "1": ["1,2", "2,3", "3,4"],
+ "2": ["4,6", "4,5"]
+ },
 
-	"1": {
-		"0": ["8,6", "1,2"],
-		"1": ["1,8", "7,3", "3,5"],
-		"2": ["2,6", "3,4"]
-	}
-}
-*/
+ "1": {
+ "0": ["8,6", "1,2"],
+ "1": ["1,8", "7,3", "3,5"],
+ "2": ["2,6", "3,4"]
+ }
+ }
+ */
 
-var mydata = require('./cis.json');
+var iter_res_dict = require('./cis.json');
+var $ = require("jquery");
 
-for(key1 in mydata){
+$.each(iter_res_dict, function (iteration_id, community_dict) {
+    setTimeout(function () {
+        console.log("iter id:" + iteration_id);
+        console.log("community_list:" + JSON.stringify(community_dict));
+        $("#global").append("<div id='iter" + iteration_id + "'></div>");
 
-	console.log("Entered key1: " +key1);
-	//graph = new Graph();
-	for(key2 in mydata[key1]){
+        $.each(community_dict, function (comm_id, edge_list) {
+            var graph = new Graph();
 
-		console.log("Entered key2: " +key2);
-		console.log("len: " +mydata[key1][key2].length);
-		for (var i = 0; i < mydata[key1][key2].length; i++) {
+            $.each(edge_list, function (idx) {
+                var edge = edge_list[idx].split(',');
+                console.log('edge:' + JSON.stringify(edge));
+                graph.addEdge(edge[0], edge[1]);
+            });
 
-			console.log("Entered i-loop: "+i);
+            var layouter = new Layout(graph);
+            layouter.layout();
 
-			var mydatasplit = mydata[key1][key2][i].split(',');
-			graph.addEdge(mydatasplit[0].toString(), mydatasplit[1].toString());
-			console.log(mydatasplit[0].toString(), mydatasplit[1].toString());
-		}
-
-	    var layout = new Layout(graph);
-	 	var renderer = new Renderer('#paper', graph, 400, 300);
-		renderer.draw()
-		graph = new Graph();
-	 	}
-
-		
-	var currentTime = new Date().getTime();
-	while (currentTime + 2000 >= new Date().getTime()) {
-	}
-	
+            var renderer = new Renderer("#iter" + iteration_id, graph, 400, 300);
+            renderer.draw();
+        });
+    }, 3000)
+});
 
 
-	// var layout = new Layout(graph);
-	// var renderer = new Renderer('#paper', graph, 400, 300);
-	// renderer.draw()
-	// graph = new Graph();	
-	
- // 	var layout = new Layout(graph);
- // 	var renderer = new Renderer('#paper', graph, 400, 300);	
- // 	renderer.draw()	
- //		graph = new Graph();
- 	
-}
