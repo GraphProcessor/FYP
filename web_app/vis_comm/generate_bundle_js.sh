f8aca7431a04cc9aa033437b0dfbed9ca2b8cfbe
nikitabatra <nikitabatra@gmail.com>
2017-04-19 17:20:40 +0800

    Update generate_bundle_js.sh

--- web_app/vis_comm/generate_bundle_js.sh
+++ web_app/vis_comm/generate_bundle_js.sh
@@ -1,2 +1,5 @@
 browserify views/browser_side_scripts/index.js -o public/javascripts/bundle_index.js
 browserify views/browser_side_scripts/cis_karate.js -o public/javascripts/bundle_cis_karate.js
+browserify views/browser_side_scripts/demon_karate.js -o public/javascripts/bundle_demon_karate.js
+browserify views/browser_side_scripts/cis_collab.js -o public/javascripts/bundle_cis_collab.js
+browserify views/browser_side_scripts/demon_collab.js -o public/javascripts/bundle_demon_collab.js
